af3c9f575a7ffae51ba3bfcc0b049da966b52494
binayrijal <binayrijal22@gmail.com>
2024-01-11 11:13:42 +0545

    set route for different navbar

--- Hakathon/hackthonapp/views.py
+++ Hakathon/hackthonapp/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 def index(request):
  
     return render(request,'index.html')
+
+def Contactus(request):
+    return render (request,'Contactus.html')
+
+def Aboutus(request):
+    return render (request,'Aboutus.html')
+
+def Signup(request):
+    return render (request,'Signup.html')
+
+def profile(request):
+    return render (request,'profile.html')
+
+def services(request):
+    return render (request,'services.html')
