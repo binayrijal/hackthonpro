7a2847cb31263ec34b61a54841876b623f805044
binayrijal <binayrijal22@gmail.com>
2024-01-12 10:30:51 +0545

    database change

--- Hakathon/hackthonapp/views.py
+++ Hakathon/hackthonapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegistrationForm
 from django.contrib import messages
+from django.shortcuts import render,get_object_or_404
+from .models import service,service_name
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def index(request):
@@ -32,4 +35,34 @@ def profile(request):
 
 def services(request):
     
-    return render (request,'services.html')
+    if request.method=="POST":
+       formdata=request.POST.get('search')
+       print(formdata)
+       if formdata:
+        selected_service = get_object_or_404(service, category=formdata)
+        selected_servicename = service_name.objects.filter(service=selected_service)
+        servicename=service_name.objects.get(id=formdata)
+        all_methods=[]
+        if servicename.methods:
+            splitmethod=servicename.methods.split(',')
+            all_methods.extend(splitmethod)
+            data={
+               'all_methods':all_methods
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse("error")
+       else:
+         return render(request,'service.html')
+        
+        
+    else:
+          return HttpResponse("no data is selected")
+
+
+
+
+
+    # Creating a list to hold methods from all service_name instances
+    
+     
