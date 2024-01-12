9942a96c47ff99986549e126936ad54d2ed76a3c
binayrijal <binayrijal22@gmail.com>
2024-01-12 13:08:56 +0545

    data base added

--- Hakathon/hackthonapp/views.py
+++ Hakathon/hackthonapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegistrationForm
 from django.contrib import messages
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,JsonResponse
+from .models import service,service_name
 
 # Create your views here.
 def index(request):
@@ -31,5 +34,33 @@ def profile(request):
     return render (request,'profile.html')
 
 def services(request):
+    if request.method=="POST":
+       formdata=request.POST.get('category')
+       
+       if formdata:
+        selected_service = get_object_or_404(service, category=formdata)
+        selected_servicename = service_name.objects.filter(service=selected_service)
+        all_methods=[]
+        for s in selected_servicename:
+          splitmethod=s.methods.split(',')
+          all_methods.extend(splitmethod)
+          return render(request,'services.html',{'selected_service':selected_service,'selected_servicename':selected_servicename,'data':formdata,'all_methods':all_methods})
+        
+       else:
+          JsonResponse("error in selected file")
+          
+
+
+
+    else:
+     services=service.objects.all()
+     all_service_names = service_name.objects.all()
+
+    # Creating a list to hold methods from all service_name instances
     
-    return render (request,'services.html')
+     return render(request,'services.html',{
+        'all_service_names':all_service_names,
+        'services':services,
+     })
+    
+   
