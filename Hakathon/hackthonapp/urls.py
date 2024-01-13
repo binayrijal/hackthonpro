d6cc45519e60a9390bf77bd7703ba5037bf7f5af
binayrijal <binayrijal22@gmail.com>
2024-01-13 08:16:29 +0545

    added files

--- Hakathon/hackthonapp/urls.py
+++ Hakathon/hackthonapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .forms import UserLoginForm,UserPasswordResetForm,MySetPasswordForm
-from .views import text_to_speech
+
 
 urlpatterns = [
     path('',views.index,name='index'),
